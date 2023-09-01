11f3812f6655fe2684fa7848d61fddd77c00851e
nskmikhey <NskMikhei@yandex.ru>
2023-09-01 15:39:50 +0700

    изменить readme

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,17 +1,13 @@
-/** Объект настроек для работы с API
- * @type {{headers: {"Content-Type": string}, serverURL: string}}
- */
+// Объект настроек для работы с API
+
 export const apiSettings = {
-    serverURL: "http://api.nskmikhey.nomoredomainsicu.ru",
+    serverURL: "https://api.nskmikhey.nomoredomainsicu.ru",
     headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
     },
 };
 
-/** Объект с ошибками сервера
- * @type {{"400": string, "401": string}}
- */
 export const SERVER_ERRORS = {
     400: "Одно из полей не заполнено или не прошло валидацию.",
     401: "Введен неверный email или пароль.",
